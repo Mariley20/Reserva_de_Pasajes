62d64c49a9dee9e4b2b1cb5db7967fb4e62ae7bc
Mariley20 <mariley2018@gmail.com>
2017-09-24 23:31:39 -0500

    identacion de codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,19 @@ const busMapa = function () {
     let cantidadAsiento = 40;
     let columnas = 4;
     let filas = cantidadAsiento / columnas;
-    let asientosHTML = "", numeroDeAsiento = 1;
+    let asientosHTML = "",
+        numeroDeAsiento = 1;
     for (let i = 1; i <= filas; i++) {
         asientosHTML += `<div class='row' id='fila${i}'>`;
-        
+
         for (let j = 0; j < columnas; j++) {
             asientosHTML += `<div class='col col-xl-2' id='${numeroDeAsiento}'>${numeroDeAsiento}</div>`;
             numeroDeAsiento += 1;
             (j == 1) ? asientosHTML += "<div class='col col-xl-1'></div>": asientosHTML += "";
-            
+
         }
         asientosHTML += "</div>";
-        
+
     }
     return asientosHTML;
 }
@@ -38,7 +39,7 @@ const reserva = {
             estado: true
         }
     ],
-    dniBuscado : undefined,
+    dniBuscado: undefined,
     inicio: () => {
         $('#bus').html(busMapa);
         reserva.colorearAsientos();
@@ -52,16 +53,16 @@ const reserva = {
         let nro = event.target.textContent;
         let clase = event.target.classList[2];
         $('#nro_Asiento').val(nro);
-        if(clase != undefined){
-           let asiento = reserva.pasajeros.filter((elemento,i) => {
+        if (clase != undefined) {
+            let asiento = reserva.pasajeros.filter((elemento, i) => {
                 return elemento.nroAsiento == nro;
-           });
+            });
             $('#nombreApellido').val(asiento[0].nombre);
             $('#dni').val(asiento[0].dni);
         }
     },
     guardarDatos: () => {
-        let clase = $('#'+$('#nro_Asiento').val())[0].classList[2];
+        let clase = $('#' + $('#nro_Asiento').val())[0].classList[2];
         console.log(clase)
         if ($('#nro_Asiento').val() != "" && $('#nombreApellido').val() != "" && $('#dni').val() != "" && clase != 'reservado') {
             let datos = {
@@ -74,15 +75,16 @@ const reserva = {
             reserva.pasajeros.push(datos);
             reserva.colorearAsientos();
             reserva.limpiarInputs();
-        } if(clase == 'reservado'){
+        }
+        if (clase == 'reservado') {
             $('#alerta').html(`<div class="alert alert-danger" role="alert">Eliminar Reserva para Agregar</div>`)
         }
-        
+
     },
     mostrarLista: () => {
         let lista = "";
         reserva.pasajeros.map((elemento) => {
-           lista += `<div class='row'>\
+            lista += `<div class='row'>\
             <div class='col-sm-2'>${elemento.nroAsiento}</div>\
             <div class='col-sm-5'>${elemento.nombre}</div>\
             <div class='col-sm-4'>${elemento.dni}</div>\
@@ -92,7 +94,7 @@ const reserva = {
     },
     buscarDNI: () => {
         let dni = $('#buscarDni').val();
-        reserva.dniBuscado = reserva.pasajeros.filter((elemento,i) => {
+        reserva.dniBuscado = reserva.pasajeros.filter((elemento, i) => {
             return elemento.dni == dni;
         });
         if (reserva.dniBuscado.length != 0) {
@@ -109,27 +111,24 @@ const reserva = {
         let nro = $('#nro_Asiento').val();
         let indice;
         reserva.pasajeros.map((elemento, i) => {
-            return (nro == elemento.nroAsiento)? indice = i : '';
+            return (nro == elemento.nroAsiento) ? indice = i : '';
         });
         //console.log(indice);
-        $('#'+reserva.pasajeros[indice].nroAsiento).removeClass('reservado')
+        $('#' + reserva.pasajeros[indice].nroAsiento).removeClass('reservado')
         reserva.pasajeros.splice(indice, 1);
         reserva.colorearAsientos();
         reserva.limpiarInputs();
 
     },
-    limpiarInputs : () => {
+    limpiarInputs: () => {
         $('#nro_Asiento').val('')
         $('#nombreApellido').val('');
         $('#dni').val('');
     },
-    colorearAsientos : () => {
+    colorearAsientos: () => {
         reserva.pasajeros.map((elemento, i) => {
-            return $('#'+elemento.nroAsiento).addClass('reservado');
+            return $('#' + elemento.nroAsiento).addClass('reservado');
         });
-    },
-    noDuplicadosAsientos : () => {
-        
     }
- }
+}
 $(document).ready(reserva.inicio);
